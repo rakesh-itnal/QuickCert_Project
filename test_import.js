72b7aabd7a221676c9bd1771fa601e935b6929c8
rakesh-itnal <rakeshitnal6@gamil.com>
2026-09-26 00:53:26 +0530

    Removed unnecessary test scripts and scratch files

--- test_import.js
+++
@@ -1,2 +0,0 @@
-const { importStudents } = require('./src/app/actions/student-actions');
-// Actually, since it's a server action, I'll just write a quick script using prisma to check what happens.
--- test_kannada.js
+++
@@ -1,1 +0,0 @@
-const { generateCertificatePDF } = require('./src/lib/pdf-generator.ts'); // Wait, need to use ts-node or just check the server log
